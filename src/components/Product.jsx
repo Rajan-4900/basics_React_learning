50b74bcf76456ec49ffc00b6591a3ce219ca7738
Rajan <146510886+Rajan-4900@users.noreply.github.com>
2026-04-14 23:23:17 +0530

    writing the props in product.jsx in function we can mention the name created in the parent app.jsx file

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const Product = (props) => {
+const Product = (name,price,color) => {
   return (
     <div>
-        <h1>Brand - {props.name}</h1>
-        <h1>Price - {props.price}</h1>
-        <h1>Color - {props.color}</h1>
+        <h1>Brand - {name}</h1>
+        <h1>Price - {price}</h1>
+        <h1>Color - {color}</h1>
 
     </div>
   )
