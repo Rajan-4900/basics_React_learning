c2126a76f0db5ef57294049eceb26da1106280be
Rajan <146510886+Rajan-4900@users.noreply.github.com>
2026-04-16 23:17:19 +0530

    code updated in the items.jsx

--- src/components/Items.jsx
+++ src/components/Items.jsx
@@ -2,20 +2,26 @@ import React from 'react'
 
 const Items = () => {
     const age = 20;
+    const adhar = false;
+    const gmail = true;
     return (
-        <div>
-            {
-                (age >= 18) ? (
+        <>
+            <div>
+                {/* {age >= 18 ? (
                     <>
-                        <h1>You Are Eligible to vote</h1>
+                    <h1>You Are Eligible to vote</h1>
                     </>
-                ) : (
-                    <>
+                    ) : (
+                        <>
                         <h1>You Are Not Eligible to vote</h1>
-                    </>
-                )
-            }
-        </div>
+                        </>
+                        )
+                        } */}
+            </div>
+
+            {adhar && <h1>You can Open Bank Account</h1>}
+            {gmail && <h1>You can make YouTube Channel</h1>}
+        </>
     )
 }
 
