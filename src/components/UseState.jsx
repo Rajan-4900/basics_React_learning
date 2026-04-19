ae20a0cb241936c069a7650e3fb3b6d5d14904dc
Rajan <146510886+Rajan-4900@users.noreply.github.com>
2026-04-19 22:58:15 +0530

    in the component we use only state-variable rather than normal variable in react this is a state variable example

--- src/components/UseState.jsx
+++ src/components/UseState.jsx
@@ -1,22 +1,27 @@
 import React from 'react'
+import { useState } from 'react'; // it is function which is used to create state variable in functional component
 
 const Usestate = () => {
-    let counter = 0;
+    // let counter = 0;
+
+    // this is a state variable 
+    const [counter, setCounter] = useState(0); // it returns an array of two elements first is state variable and second is function to update the state variable
 
     const increment = () => {
-        counter++;
-        console.log(counter);
+        // counter++;
+        // console.log(counter);
+        setCounter(counter + 1); // it is used to update the state variable and it takes the new value of the state variable as an argument
     }
     const decrement = () => {
-        counter--;
-        console.log(counter);
+        // counter--;
+        // console.log(counter);
+        setCounter(counter - 1); // it is used to update the state variable and it takes the new value of the state variable as an argument
     }
 
   return (
     <>
     <h1>Counter = {counter}</h1>
     <br/>
-
     <button onClick={increment}>Increase</button>
     <button onClick={decrement}>Decrease</button>
     </>
