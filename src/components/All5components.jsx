bf22052ff2908941ae75d5da1e997d5b44fbf899
Rajan <146510886+Rajan-4900@users.noreply.github.com>
2026-04-20 23:14:08 +0530

    code is updated

--- src/components/All5components.jsx
+++ src/components/All5components.jsx
@@ -29,22 +29,24 @@ const All5components = () => {
                 //   borderRadius: '5px'
                 // }}
 
-                // style={Style} // it is used to apply the internal CSS styling
+                style={Style} // it is used to apply the internal CSS styling
 
-                // className="container" // it used to apply external CSS Styling
+                className="container" // it used to apply external CSS Styling
 
                 className="con" // in external css which is created by new product component
             >
                 <h1>Brand - {name}</h1>
                 <h3>Price - {price}</h3>
                 <h3>Color - {color}</h3>
-                {/* {data.brand && (
-        <>
-        <p>Brand - {data.brand}</p>
-        <p>Cost - {data.cost}</p>
-        <p>Color - {data.colour}</p>
-      </>
-      )}       */}
+
+                // this is used to check if the data object has the brand property then it will display the brand, cost and color of the product otherwise it will not display anything
+                {data.brand && (
+                    <>
+                        <p>Brand - {data.brand}</p>
+                        <p>Cost - {data.cost}</p>
+                        <p>Color - {data.colour}</p>
+                    </>
+                )}
             </div>
         );
     };
@@ -60,8 +62,8 @@ const All5components = () => {
         const increment = () => {
             // counter++;
             // console.log(counter);
-            // setCounter(counter + 1); // it is used to update the state variable and it takes the new value of the state variable as an argument
-            setCounter(prevState => prevState + 1); // it is used to update the state variable and it takes a function as an argument which returns the new value of the state variable
+            setCounter(counter + 1); // it is used to update the state variable and it takes the new value of the state variable as an argument
+            // setCounter(prevState => prevState + 1); // it is used to update the state variable and it takes a function as an argument which returns the new value of the state variable
         }
 
         // we can also use the value to increase the initial value of the state variable
@@ -71,8 +73,8 @@ const All5components = () => {
         const decrement = () => {
             // counter--;
             // console.log(counter);
-            // setCounter(counter - 1); // it is used to update the state variable and it takes the new value of the state variable as an argument
-            setCounter(prevState => prevState - 1); // it is used to update the state variable and it takes a function as an argument which returns the new value of the state variable
+            setCounter(counter - 1); // it is used to update the state variable and it takes the new value of the state variable as an argument
+            // setCounter(prevState => prevState - 1); // it is used to update the state variable and it takes a function as an argument which returns the new value of the state variable
         }
 
         return (
@@ -80,7 +82,7 @@ const All5components = () => {
                 <h1>Counter = {counter}</h1>
                 <br />
                 <button onClick={increment}>Increment</button>
-                <button onClick={() => increase(100)}>Increase</button>
+                <button onClick={() => increase(100)}>Increase</button> // it is used to increase the counter value by 100 when the button is clicked
                 <button onClick={decrement}>Decrement</button>
             </>
         )
@@ -130,7 +132,7 @@ const All5components = () => {
 }
 
 
-export default All5components
+export default All5components;
 export default Product;
-export default Usestate
-export default Events
+export default Usestate;
+export default Events;
