d958920f7e0315dff57a0df4ef5f6c46404ddb1e
Rajan-91480 <rajan91480@gmail.com>
2026-04-26 13:47:07 +0530

    active persons are there in the screen

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -2,19 +2,23 @@ import React from 'react'
 
 const Filter = () => {
     const person = [
-        {id:1, name:"Raj", active:true},
-        {id:2, name:"Raju", active:false},
-        {id:3, name:"bot", active:true},
-        {id:4, name:"detto", active:false},
+        { id: 1, name: "Raj", active: true },
+        { id: 2, name: "Raju", active: false },
+        { id: 3, name: "bot", active: true },
+        { id: 4, name: "detto", active: false },
     ];
 
-    const active_person = person.filter(data => !data.active)
-    console.log(active_person);
-  return (
-    <div>
-    <h1>Filter</h1>
-    </div>
-  )
+    // const active_person = person.filter(data => !data.active)
+    // console.log(active_person);
+    return (
+        <div>
+            {person.filter((data) => data.active).map((item) => ( // this is for diaplying in the screen for true 
+                <div key={item.id}>
+                    <h1>{item.name}</h1>
+                </div>
+            ))}
+        </div>
+    )
 }
 
 export default Filter
