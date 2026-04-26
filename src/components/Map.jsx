0effe28a542ba181edc1f35f25e2b5f1c360aebd
Rajan-91480 <rajan91480@gmail.com>
2026-04-26 12:28:44 +0530

    new map is created by phones function with style added

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,16 +1,30 @@
 import React from 'react'
 
 const Map = () => {
-    const data = ["Google", "Microsoft", "Apple", "Amazon"]
-  return (
-    <div>
-        {data.map((element, index) => (
-            <div key = {index}>
-                <h1>{element}</h1>
+    // const data = ["Google", "Microsoft", "Apple", "Amazon"]
+    const smartPhone = [
+        { id: 1, model: "Iphone 17 pro", img : "https://m.media-amazon.com/images/I/71JGCn1z1TL._AC_UY327_FMwebp_QL65_.jpg", },
+        { id: 2, model: "Iphone 16 pro", img : "https://m.media-amazon.com/images/I/71JGCn1z1TL._AC_UY327_FMwebp_QL65_.jpg", },
+        { id: 3, model: "Iphone 15 pro", img : "https://m.media-amazon.com/images/I/71JGCn1z1TL._AC_UY327_FMwebp_QL65_.jpg", }
+    ];
+    return (
+        <>
+        <div>
+        {smartPhone.map((data) => (
+            <div key={data.id}>
+                <img style={{ width: "150px" }} src={data.img} alt="" />
+                <h3>{data.model}</h3>
             </div>
         ))}
-    </div>
-  )
+        </div>
+            {/* {data.map((element, index) => (
+                <div key={index}>
+                    <h1>{element}</h1>
+                </div>
+            ))} */}
+
+        </>
+    )
 }
 
-export default Map
+export default Map;
