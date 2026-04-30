27af711b1ebd72fd7f3c935c1e6c93b131ad0fcc
Rajan-91480 <rajan91480@gmail.com>
2026-04-30 22:58:33 +0530

    we have set the value to be changed in the iput box using "onChange={}" event function

--- src/components/FormHandling.jsx
+++ src/components/FormHandling.jsx
@@ -1,30 +1,30 @@
 import React from 'react'
 
 const FormHandling = () => {
-    const [name, setname] = useState('')
-    const [email, setemail] = useState('')
-    const [password, setepassword] = useState('')
-  return (
-    <>
-    <form>
-    <div>
-        Name :- <input type="text" value={name} placeholder='Enter Your Full Name' />
-    </div>
-    <br />
-    <div>
-        Email :- <input type="email" value={email} placeholder='Enter Your Email' />
-    </div>
-    <br />
-    <div>
-        Password :- <input type="password" value={password} placeholder='Enter Your Password' />
-    </div>
-    <br />
-    <div>
-    <input type="submit" value="submit" />
-    </div>
-    </form>
-    </>
-  )
+    const [name, setname] = useState("")
+    const [email, setemail] = useState("")
+    const [password, setepassword] = useState("")
+    return (
+        <>
+            <form>
+                <div>
+                    Name :- <input onChange={(name)=>setname(name.target.value)} type="text" value={name} placeholder='Enter Your Full Name' />
+                </div>
+                <br />
+                <div>
+                    Email :- <input onChange={(email)=>setemail(email.target.value)} type="email" value={email} placeholder='Enter Your Email' />
+                </div>
+                <br />
+                <div>
+                    Password :- <input onChange={(pass)=>setepassword(pass.target.value)} type="password" value={password} placeholder='Enter Your Password' />
+                </div>
+                <br />
+                <div>
+                    <input type="submit" value="submit" />
+                </div>
+            </form>
+        </>
+    )
 }
 
 export default FormHandling
