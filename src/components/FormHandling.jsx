f96be7a0d5dfcb05cce96864eeac971f13b8a708
Rajan-91480 <rajan91480@gmail.com>
2026-04-30 23:10:14 +0530

    we have created a "stop browser auto reload using onSubmitHandler and preventDefualt" using this we made on console display with not auto reload  when submiting

--- src/components/FormHandling.jsx
+++ src/components/FormHandling.jsx
@@ -1,22 +1,28 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const FormHandling = () => {
-    const [name, setname] = useState("")
-    const [email, setemail] = useState("")
-    const [password, setepassword] = useState("")
+    const [name, setname] = useState("");
+    const [email, setemail] = useState("");
+    const [password, setepassword] = useState("");
+
+    // to stop the browser default reload option automaticaly
+    const onSubmitHandler = (e) => {
+        e.preventDefault();
+        console.log(name,email,password)
+    }
     return (
         <>
-            <form>
+            <form onSubmit={onSubmitHandler}>
                 <div>
-                    Name :- <input onChange={(name)=>setname(name.target.value)} type="text" value={name} placeholder='Enter Your Full Name' />
+                    Name :- <input onChange={(e)=>setname(e.target.value)} type="text" value={name} placeholder='Enter Your Full Name' />
                 </div>
                 <br />
                 <div>
-                    Email :- <input onChange={(email)=>setemail(email.target.value)} type="email" value={email} placeholder='Enter Your Email' />
+                    Email :- <input onChange={(e)=>setemail(e.target.value)} type="email" value={email} placeholder='Enter Your Email' />
                 </div>
                 <br />
                 <div>
-                    Password :- <input onChange={(pass)=>setepassword(pass.target.value)} type="password" value={password} placeholder='Enter Your Password' />
+                    Password :- <input onChange={(e)=>setepassword(e.target.value)} type="password" value={password} placeholder='Enter Your Password' />
                 </div>
                 <br />
                 <div>
